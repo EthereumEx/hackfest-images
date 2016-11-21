7bf61b199dbd9b81a01e773851a187a79c5beefc
Eric Maino <ericmai@microsoft.com>
2016-11-21 03:14:10 +0000

    Updating with miner and name options

--- geth-node/runGeth.sh
+++ geth-node/runGeth.sh
@@ -4,8 +4,8 @@ GENESIS=/home/geth/genesis.json
 DATADIR=/home/geth/.geth
 RPCPORT=8545
 RPCHOST=0.0.0.0
-RPCAPI=all
 GETHPORT=30303
+GETHMINE=
 
 if [ -z "$NETWORKID" ]; then
   echo "No NETWORKID was supplied"
@@ -17,13 +17,28 @@ if [ -z "$GENESIS" ]; then
   exit 1
 fi
 
+if [ -z "$NODE_NAME" ]; then
+  echo "No NODE_NAME was supplied"
+  exit 1
+fi
+
+if [ -z "$ENABLE_MINER" ]; then
+  if [ -z "$MINER_ADDRESS" ]; then
+    echo "No MINER_ADDRESS was supplied"
+    exit 1
+  fi
+GETHARGS=--mine --etherbase $MINER_ADDRESS
+fi
+
 if [ ! -d "$DATADIR/chaindata" ]; then
   geth --datadir $DATADIR init $GENESIS
 fi
 
 geth --datadir $DATADIR \
+        --identity $NODE_NAME
         --rpc --rpcport $RPCPORT --rpcaddr $RPCHOST \
         --fast \
         --networkid $NETWORKID \
-        --port $GETHPORT
+        --port $GETHPORT \
+        $GETHARGS
 
