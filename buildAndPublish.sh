f1adcf6fb504052fdcea8ff26111eb2083cd1938
Eric Maino <eric.maino@microsoft.com>
2016-11-27 08:19:41 +0000

    Cleaning up images a bit

--- buildAndPublish.sh
+++ buildAndPublish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ORG=ethereumex
-docker build -t $ORG/$1 $1/.
+docker build -t $ORG/$1 $2 $1/.
 docker push $ORG/$1
--- updateAndPublish.sh
+++ updateAndPublish.sh
@@ -4,7 +4,6 @@ docker pull ubuntu
 docker pull ethereume/client-go
 sh buildAndPublish.sh node
 sh buildAndPublish.sh geth-dev
-sh buildAndPublish.sh geth-stats
-sh buildAndPublish.sh geth-node
-sh buildAndPublish.sh boot-registrar
-sh buildAndPublish.sh eth-stats-dashboard
+sh buildAndPublish.sh geth-node --no-cache
+sh buildAndPublish.sh boot-registrar --no-cache
+sh buildAndPublish.sh eth-stats-dashboard --no-cache
