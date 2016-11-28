252d13ea5385e318e1d9e66f608973b7292ccee8
Eric Maino <eric.maino@microsoft.com>
2016-11-28 00:12:07 +0000

    Removing images and scripts no longer needed

--- updateAndPublish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker pull ubuntu
-docker pull ethereume/client-go
-sh buildAndPublish.sh node
-sh buildAndPublish.sh geth-dev
-sh buildAndPublish.sh geth-node --no-cache
-sh buildAndPublish.sh boot-registrar --no-cache
-sh buildAndPublish.sh eth-stats-dashboard --no-cache
